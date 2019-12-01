255c8e322f8e3563b16c1df683813c157c900fd2
Ivan Avdonin <avdonin@yahoo.com>
2019-12-01 19:41:16 +0300

    fix all bugs caused by the last commit

--- square.py
+++ square.py
@@ -19,16 +19,16 @@ class Square:
 class Hero(Square):
     ''' The Hero is a creature which is managed by the user '''
     
-    INITIAL_STEP_TIME = 0.15
-    FASTEST_STEP_TIME = 0.04
-    STEP_TIME_ACCELERATION = 0.01
-
     def __init__(self, x, y, color):
+        self.INITIAL_STEP_TIME = 0.22
+        self.FASTEST_STEP_TIME = 0.04
+        self.STEP_TIME_ACCELERATION = 0.03
+
         Square.__init__(self, x, y, color)
 
         self.STEP_TIME = self.INITIAL_STEP_TIME
         self.last_step_time = time.time()
-        self._next_step = None
+        self.__next_step = None
 
 
     # The Hero moves, moves and takes eggs, moves the pseudo_cutter parts 
@@ -36,7 +36,8 @@ class Hero(Square):
         self.next_step = None
         if time.time() - self.last_step_time > self.STEP_TIME:
             is_pressed = pygame.key.get_pressed()
-            if is_pressed: 
+            
+            if is_pressed[K_LEFT] or is_pressed[K_RIGHT] or is_pressed[K_UP] or is_pressed[K_DOWN]:
                 self.next_step = [self.x, self.y]
                 if self.STEP_TIME-self.STEP_TIME_ACCELERATION > self.FASTEST_STEP_TIME: 
                     self.STEP_TIME -= self.STEP_TIME_ACCELERATION
@@ -63,18 +64,20 @@ class Hero(Square):
             step_is_made = eggs[4].update(self) or step_is_made
             step_is_made = pseudo_cutter.update(field, self, eggs, cutter, snake) or step_is_made
             if not step_is_made and self.next_step != None: 
-                if field.is_valid(self.next_step[0], self.next_step[1]):            
-                    self.make_step()
+                if field.is_valid(self.next_step[0], self.next_step[1]):
+                    # the next condition will not allow diffraction-like behaviour while moving diagonally
+                    if (field.is_valid(self.x, self.next_step[1]) or field.is_valid(self.next_step[0], self.y)):        
+                        self.make_step()
+                        eggs[0].update_times() # Updates times of the last hero steps
                 # the next conditions will allow to moves along walls
                 elif field.is_valid(self.next_step[0], self.y):
                     self.next_step = [self.next_step[0], self.y]
                     self.make_step()
+                    eggs[0].update_times() # Updates times of the last hero steps
                 elif field.is_valid(self.x, self.next_step[1]):
                     self.next_step = [self.x, self.next_step[1]]
                     self.make_step()
-
-                
-                eggs[0].update_times() # Updates times of the last hero steps
+                    eggs[0].update_times() # Updates times of the last hero steps
 
         if time.time() - self.last_step_time > 2*self.STEP_TIME:
             self.STEP_TIME = self.INITIAL_STEP_TIME
@@ -90,7 +93,7 @@ class Box(Square):
     def update(self, field, hero):
         ''' Returns if hero made a step'''
         
-        if hero.next_step != None and (self.x, self.y) == hero.next_step:
+        if hero.next_step != None and [self.x, self.y] == hero.next_step:
             # Checks if the next cell for the Box is valid 
             next_x = 2 * self.x - hero.x
             next_y = 2 * self.y - hero.y
